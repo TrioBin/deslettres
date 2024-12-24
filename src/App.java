afab41a22e22aad0b9d5cb4736cc3b81e21713d2
FatlumAL <fatlum.muzaqi@insa-strasbourg.fr>
2024-12-24 16:06:49 +0100

    Refactor main method to call tirages and validité functions for letter selection and validation

--- src/App.java
+++ src/App.java
@@ -1,13 +1,22 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        consonnes();
+        char[] lettres = new char[10]; // tableau qui contient les 10 lettres tirées aléatoirement
+        tirages(lettres);
+        System.out.println();
+        validité(lettres);
     }
 
-    public static void consonnes() {
-        int i, indice;
+    public static void tirages(char[] lettres_finales) {
+        int i, j, l, indice;
+        char k;
         char[] tirage_consonnes = new char[5]; // tableau qui va contenir les 5 consonnes tirées aléatoirement
+        char[] tirage_voyelles = new char[3]; // tableau qui va contenir les 3 voyelles tirées aléatoirement
+        char[] tirage_lettres_supp = new char[2]; // tableau qui va contenir les 2 lettres supplémentaires tirées
+                                                  // aléatoirement
         char[] consonnes1 = { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W',
                 'X', 'Z' }; // tableau des consonnes
         ArrayList<Character> consonnes2 = new ArrayList<Character>(); // On converti notre tableau consonnes1 en
@@ -25,14 +34,133 @@ public class App {
             indice = Lire.i();
             if (indice < 0 || indice > 19) { // On vérifie que l'indice saisi par l'utilisateur est bien compris entre 0
                                              // et 19
-                System.out.println("L'indice doit être compris entre 0 et 19");
+                System.out.println("Erreur : L'indice doit être compris entre 0 et 19");
             }
             tirage_consonnes[i] = consonnes2.get(indice); // On ajoute la consonne choisie par l'utilisateur dans le
                                                           // tableau tirage_consonnes
-            java.util.Collections.shuffle(consonnes2);// On remélamnge la liste consonnes2 de manière aléatoire
+            java.util.Collections.shuffle(consonnes2); // On remélamnge la liste consonnes2 de manière aléatoire
         }
         for (i = 0; i < 5; i++) {
             System.out.print(tirage_consonnes[i] + " "); // On affiche les 5 consonnes tirées aléatoirement
         }
+        System.out.println();
+        char[] voyelles1 = { 'A', 'E', 'I', 'O', 'U', 'Y' }; // tableau des voyelles
+        ArrayList<Character> voyelles2 = new ArrayList<Character>(); // On converti notre tableau voyelles1 en
+                                                                     // ArrayList afin de pouvoir mélanger la liste de
+                                                                     // manière aléatoire
+        for (char c : voyelles1) {
+            voyelles2.add(c);
+        }
+        java.util.Collections.shuffle(voyelles2); // On mélange la liste voyelles2 de manière aléatoire en utilisant
+                                                  // la méthode shuffle de la classe Collections
+        for (l = 0; l < 3; l++) {
+            System.out.println("Sélectionner un indice entre 0 et 5"); // On demande à l'utilsateur de saisir un indice
+                                                                       // entre 0 et 5 afin de choisir une voyelle de la
+                                                                       // liste
+            indice = Lire.i();
+            if (indice < 0 || indice > 5) {
+                System.out.println("Erreur : L'indice doit être compris entre 0 et 5");
+            }
+            tirage_voyelles[l] = voyelles2.get(indice); // On ajoute la voyelle choisie par l'utilisateur dans le
+                                                        // tableau tirage_voyelles
+            java.util.Collections.shuffle(voyelles2); // On remélamnge la liste voyelles2 de manière aléatoire
+        }
+        for (l = 0; l < 3; l++) {
+            System.out.print(tirage_voyelles[l] + " "); // On affiche les 3 voyelles tirées aléatoirement
+        }
+        System.out.println();
+        ArrayList<Character> alphabet = new ArrayList<Character>(); // On crée une liste alphabet qui va contenir les 26
+                                                                    // lettres de l'alphabet
+        for (k = 'A'; k <= 'Z'; k++) { 
+            alphabet.add(k);
+        }
+        java.util.Collections.shuffle(alphabet); // On mélange la liste alphabet de manière aléatoire
+        for (j = 0; j < 2; j++) {
+            System.out.println("Sélectionner un indice entre 0 et 25"); // On demande à l'utilsateur de saisir un indice
+                                                                        // entre 0 et 25 afin de choisir une lettre de
+                                                                        // la liste
+            indice = Lire.i();
+            if (indice < 0 || indice > 25) {
+                System.out.println("Erreur : L'indice doit être compris entre 0 et 25");
+            }
+            tirage_lettres_supp[j] = alphabet.get(indice); // On ajoute la lettre choisie par l'utilisateur dans le
+                                                           // tableau tirage_lettres_supp
+            java.util.Collections.shuffle(alphabet); // On remélamnge la liste alphabet de manière aléatoire
+        }
+        for (j = 0; j < 2; j++) {
+            System.out.print(tirage_lettres_supp[j] + " "); // On affiche les 2 lettres supplémentaires tirées
+                                                            // aléatoirement
+        }
+        System.out.println();
+        for (i = 0; i < 5; i++) {
+            lettres_finales[i] = tirage_consonnes[i];
+        }
+        for (i = 5; i < 8; i++) {
+            lettres_finales[i] = tirage_voyelles[i - 5];
+        }
+        for (i = 8; i < 10; i++) {
+            lettres_finales[i] = tirage_lettres_supp[i - 8];
+        }
+        for (i = 0; i < 10; i++) {
+            System.out.print(lettres_finales[i] + " ");
+        }
     }
+
+    public static void validité(char[] lettres) {
+        int length, i, k = 0;
+        String mot, MOT;
+        char[] mot_formé = new char[10]; // tableau qui va contenir les lettres du mot saisi par l'utilisateur
+        System.out.println("Saisir le mot le plus long possible à partir des lettres tirées");
+        mot = Lire.S();
+        length = mot.length(); // On récupère la longueur du mot saisi par l'utilisateur
+        MOT = mot; // On stocke le mot saisi par l'utilisateur dans la variable MOT
+        for (i = 0; i < length; i++) {
+            mot_formé[i] = mot.charAt(i); // On ajoute chaque lettre du mot saisi par l'utilisateur dans le tableau
+                                          // mot_formé
+        }
+        for (i = length; i < 10; i++) {
+            mot_formé[i] = '0'; // On complète le tableau mot_formé avec le charactère '0' pour avoir un tableau
+                                // de 10 cases
+        }
+        for (i = 0; i < length; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (mot_formé[i] == lettres[j]) { // On vérifie si chaque lettre du mot saisi par l'utilisateur est bien
+                                                  // présente dans le tableau lettres
+                    mot_formé[i] = '0'; // Si la lettre est présente, on la remplace par le charactère '0'
+                    lettres[j] = '0'; // On remplace la lettre du tableau lettres par le charactère '0' pour ne pas la
+                                      // réutiliser
+                }
+            }
+        }
+        System.out.println();
+        for (i = 0; i < 10; i++) {
+            if (mot_formé[i] == '0') { // On compte le nombre de charactère '0' dans le tableau mot_formé
+                k = k + 1;
+            }
+        }
+        if (k == 10) { // Si le nombre de charactère '0' est égal à 10, cela signifie que le mot saisi
+                       // par l'utilisateur
+                       // est bien formé à partir des lettres tirées
+            System.out.println(
+                    "Le mot " + MOT + " est bien formé à partir des lettres tirées et il fait " + length + " lettres");
+        } else {
+            System.out.println("Le mot " + MOT + " n'est pas formé à partir des lettres tirées");
+        }
+        String Dictionnaire = "Dictionnaire.txt";
+        try {
+            File fichier = new File(Dictionnaire); // On crée un objet File pour représenter le fichier à lire.
+            Scanner sc = new Scanner(fichier); // On crée un objet de type Scanner pour lire le fichier Dictionnaire.txt
+            while (sc.hasNextLine()){
+                String ligne = sc.nextLine(); // On lit chaque ligne du fichier Dictionnaire.txt
+                if (ligne.equals(MOT)){ // On vérifie si le mot saisi par l'utilisateur est présent dans le fichier
+                    System.out.println("Le mot " + MOT + " est bien présent dans le dictionnaire");
+                } else {
+                    System.out.println("Le mot " + MOT + " n'est pas présent dans le dictionnaire");
+                }
+            }
+            sc.close(); // On ferme le scanner
+        } catch (Exception e) {
+            System.out.println("Erreur le dictionnaire n'a pas été trouvé");
+        }
+    }   
 }
